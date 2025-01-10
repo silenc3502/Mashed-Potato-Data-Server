a83b1af68582d6bf6821186c6a92c0abd691fad4
GOEUN JU <133327235+Goeun-Ju@users.noreply.github.com>
2025-01-10 21:03:08 +0900

    fix: [주고은] 오더 아이템 범위 수정 [MPD]

--- db_mp/orders/entity/orders_items.py
+++ db_mp/orders/entity/orders_items.py
@@ -9,7 +9,7 @@ class OrdersItems(models.Model):
     orders = models.ForeignKey(Orders, related_name="items", on_delete=models.CASCADE)  # Order와 연결
     car = models.ForeignKey(Car, related_name="items", on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=20, decimal_places=2)
 
     def __str__(self):
         return f"Item: {self.quantity} x {self.price}"
